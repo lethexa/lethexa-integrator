39f410cb50d59dfb8331a77b2d80405662e872e7
Tim Leerhoff <tleerhof@web.de>
2015-08-10 20:59:12 +0200

    Algorithm and tests now working

--- test/rungekutta.js
+++ test/rungekutta.js
@@ -22,14 +22,15 @@ describe('RungeKutta2Order', function () {
     describe('#nextStep()', function () {
         it('should return the correct result', function () {
 		var rungekutta = new integrator.RungeKutta2Order( function(x, y, dy) {
-			return 3 * x*x * y;
+			return dy + 2.0 * y;
 		}); 
-		var x0 = 1, y0 = 2, dy0 = 0, h = 0.1;
+		var x0 = 0, y0 = 3, dy0 = 0, h = 0.1;
 
 		var result = rungekutta.nextStep(x0, y0, dy0, h);
 
-            	assert.equal(1.1, Math.round(result.x1 * 1000.0) / 1000.0);
-            	assert.equal(2.785, Math.round(result.y1 * 1000.0) / 1000.0);
+            	assert.equal(0.1, Math.round(result.x1 * 1000.0) / 1000.0);
+            	assert.equal(3.031, Math.round(result.y1 * 1000.0) / 1000.0);
+		assert.equal(0.633, Math.round(result.dy1 * 1000.0) / 1000.0);
         });
     });
 });
