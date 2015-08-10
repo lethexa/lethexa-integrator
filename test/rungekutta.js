cfc5d99f55cd29890ab159376194259fbef17da8
Tim Leerhoff <tleerhof@web.de>
2015-08-10 20:51:38 +0200

    Extended 2. order integrator

--- test/rungekutta.js
+++ test/rungekutta.js
@@ -21,12 +21,12 @@ describe('RungeKutta1Order', function () {
 describe('RungeKutta2Order', function () {
     describe('#nextStep()', function () {
         it('should return the correct result', function () {
-		var rungekutta = new integrator.RungeKutta1Order( function(x, y) {
+		var rungekutta = new integrator.RungeKutta2Order( function(x, y, dy) {
 			return 3 * x*x * y;
 		}); 
-		var x0 = 1, y0 = 2, h = 0.1;
+		var x0 = 1, y0 = 2, dy0 = 0, h = 0.1;
 
-		var result = rungekutta.nextStep(x0, y0, h);
+		var result = rungekutta.nextStep(x0, y0, dy0, h);
 
             	assert.equal(1.1, Math.round(result.x1 * 1000.0) / 1000.0);
             	assert.equal(2.785, Math.round(result.y1 * 1000.0) / 1000.0);
